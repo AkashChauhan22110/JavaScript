1f14466d441bcb779cdd2c2c96caab677220a9db
Akash Chauhan <akashchauhan22110@gmail.com>
2022-10-21 22:51:01 +0530

    done

---
+++ variable02.js
@@ -0,0 +1,11 @@
+var firstName = "Harry";
+console.log(firstName);
+firstName = "Harshit";
+console.log(firstName);
+// Harry
+// Harshit
+var a = 2;
+var b = 3;
+let c = a+b;
+console.log(c);
+// 5
